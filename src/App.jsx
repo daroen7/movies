b355088369f58014370482267387d030c4481fa9
Syaihan <daroen662@gmail.com>
2026-05-03 11:46:14 +0700

    first commit movie app

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react'
+import Search from './components/Search'
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTION = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500)
+  const [debouncedTerm, setDebouncedTerm] = useState('')
+
+  useDebounce(() => setDebouncedTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true)
+    setErrorMessage('')
+
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTION)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      console.log('Fetched movies:', data)
+
+      if (data.Response === 'false') {
+        setErrorMessage(data.Error || 'Gagal mengambil data film. Silakan coba lagi nanti.')
+        setMovieList([])
+        return
+      }
+      setMovieList(data.results || [])
+
+    } catch (error) {
+      console.error('Error fetching movies:', error)
+      setErrorMessage('Gagal mengambil data film. Silakan coba lagi nanti.')
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debouncedTerm)
+  }, [debouncedTerm])
+
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Temukan <span className='text-gradient'>Film</span> Favoritmu</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className='all-movies'>
+          <h2 className='mt-[40px]'>All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+            // <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map(movie => (
+                <li key={movie.id}>
+                  <MovieCard key={movie.id} movie={movie} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+
+      </div>
+    </main>
+
+  )
+}
+
+export default App
